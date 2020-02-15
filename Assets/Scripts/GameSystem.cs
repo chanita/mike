9360535a6c399e0fc83673f4f7075df24caff4bd
Chanita Air Anuwong <chanita@odds.team>
2020-02-15 17:36:46 +0700

    init game

---
+++ Assets/Scripts/GameSystem.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameSystem : MonoBehaviour
+{
+    private int score = 0;
+    private bool isPlayerWin = false;
+    public int winningScore = 500;
+    public Text scoreText;
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPlayerWin)
+        {
+            return;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.DrawRay(ray.origin, ray.direction*hit.distance, Color.green, 0.5f);
+
+                if (!hit.transform.GetComponent<Mole>().IsDead)
+                {
+                    score += 100;
+                }
+                
+                hit.transform.GetComponent<Mole>().Dead();
+                Debug.Log(message:$"Current score {score}");
+                if (score == winningScore)
+                {
+                    isPlayerWin = true;
+                    Debug.Log(message: "End");
+                    StartCoroutine(("SetWinningStage"));
+                }
+            }
+            else
+            {
+                Debug.DrawRay(ray.origin, ray.direction*100, Color.red, 0.5f);
+            }
+            
+        }
+
+        scoreText.text = $"SCORE: {score}";
+    }
+
+    IEnumerator SetWinningStage()
+    {
+        yield return new WaitForSeconds(.5f);
+        SceneManager.LoadScene("WinningScene");
+    }
+    
+}
---
+++ Assets/Scripts/Mole.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Mole : MonoBehaviour
+{
+   public AudioSource deadSound;
+   public AudioSource fleeSound;
+   private Animator _animator;
+   private AudioSource _audioSource;
+   private bool _isDead = false;
+   
+   public void Start()
+   {
+      _animator = transform.GetComponent<Animator>();
+      _audioSource = transform.GetComponent<AudioSource>();
+   }
+   public void Dead()
+   {
+      if (_isDead)
+      {
+         return;
+      }
+      _animator.SetBool("isDead", true);
+      _audioSource.Play();
+      _isDead = true;
+
+      StartCoroutine("Disappear");
+   }
+
+   IEnumerator Disappear()
+   {
+      yield return new WaitForSeconds(.5f);
+      transform.gameObject.SetActive(false);
+   }
+
+   public bool IsDead => _isDead;
+   
+}
